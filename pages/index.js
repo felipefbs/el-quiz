1070a5f62ceae03db45cb4e444ca4926884cebde
Felipe Ferreira <felipeffbs3x@gmail.com>
2021-01-25 16:41:00 -0300

    chore: exporting Widget component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
 import styled from "styled-components";
+import Widget from "../src/components/Widget";
 import db from "../db.json";
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`;
 
 const BackgroundImage = styled.div`
   background-image: url(${db.bg});
@@ -21,40 +17,6 @@ export const QuizContainer = styled.div`
   @media screen and (max-width: 500px) {
     margin: auto;
     padding: 15px;
-  }
-`;
-
-const Widget = styled.div`
-  margin-top: 24px;
-  margin-bottom: 24px;
-  border: 1px solid #4caf50;
-  border-radius: 4px;
-  background-color: #1c1814;
-  overflow: hidden;
-`;
-
-Widget.content = styled.div`
-  padding: 24px 32px 32px 32px;
-  & > *:first-child {
-    margin-top: 0;
-  }
-  & > *:last-child {
-    margin-bottom: 0;
-  }
-  ul {
-    list-style: none;
-    padding: 0;
-  }
-`;
-
-Widget.header = styled.header`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  padding: 18px 32px;
-  background-color: ${({ theme }) => theme.colors.primary};
-  * {
-    margin: 0;
   }
 `;
 
---
+++ src/components/Widget/index.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  border-radius: 4px;
+  background-color: ${({ theme }) => theme.colors.mainBg};
+  overflow: hidden;
+`;
+
+Widget.content = styled.div`
+  padding: 24px 32px 32px 32px;
+  & > *:first-child {
+    margin-top: 0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
+
+Widget.header = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 18px 32px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  * {
+    margin: 0;
+  }
+`;
+
+export default Widget;
