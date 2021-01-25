9034c166fd8cc9dde34c831db0fd349d702eded2
Felipe Ferreira <felipeffbs3x@gmail.com>
2021-01-25 16:33:56 -0300

    beauty: widget component

--- pages/index.js
+++ pages/index.js
@@ -24,10 +24,61 @@ export const QuizContainer = styled.div`
   }
 `;
 
+const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px solid #4caf50;
+  border-radius: 4px;
+  background-color: #1c1814;
+  overflow: hidden;
+`;
+
+Widget.content = styled.div`
+  padding: 24px 32px 32px 32px;
+  & > *:first-child {
+    margin-top: 0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
+
+Widget.header = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 18px 32px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  * {
+    margin: 0;
+  }
+`;
+
 export default function Home() {
   return (
     <BackgroundImage>
-      <QuizContainer>Test</QuizContainer>
+      <QuizContainer>
+        <Widget>
+          <Widget.header>
+            <h1>Teste</h1>
+          </Widget.header>
+          <Widget.content>
+            <p> Lorem ipsum dolor sit amet, consectetur adip</p>
+          </Widget.content>
+        </Widget>
+        <Widget>
+          <Widget.header>
+            <h1>Teste</h1>
+          </Widget.header>
+          <Widget.content>
+            <p> Lorem ipsum dolor sit amet, consectetur adip</p>
+          </Widget.content>
+        </Widget>
+      </QuizContainer>
     </BackgroundImage>
   );
 }
