fcdec7d9978b6c2288daf1af0177633871d91b2c
Felipe Ferreira <felipeffbs3x@gmail.com>
2021-01-25 16:18:59 -0300

    quizContainer component

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,21 @@ const BackgroundImage = styled.div`
   background-position: cover;
 `;
 
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
 export default function Home() {
-  return <BackgroundImage></BackgroundImage>;
+  return (
+    <BackgroundImage>
+      <QuizContainer>Test</QuizContainer>
+    </BackgroundImage>
+  );
 }
